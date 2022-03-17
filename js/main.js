191fc8c868ffea5e0eaac769022681d87cae851d
cochy98 <cosmoferrigno@gmail.com>
2022-03-17 15:00:29 +0100

    inizializzazione vue slider

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const carouselImages = [
+    {
+        name: 'Immagine random 1',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+        source: 'https://picsum.photos/id/0/5616/3744',
+    },
+    {
+        name: 'Immagine random 2',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+        source: 'https://picsum.photos/id/2/5616/3744',
+    },
+    {
+        name: 'Immagine random 3',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+        source: 'https://picsum.photos/id/3/5616/3744',
+    },
+    {
+        name: 'Immagine random 4',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+        source: 'https://picsum.photos/id/4/5616/3744',
+    },
+    {
+        name: 'Immagine random 5',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+        source: 'https://picsum.photos/id/1/5616/3744',
+    },
+];
+
+
+/*
+<div class="carousel-element position-relative">
+    <img src="${array[indexElementView].source}" class="my-img-carousel" alt="${array[indexElementView].name}">
+    <div class="carousel-img-description position-absolute bottom-0 end-0 text-end text-white p-4">
+        <h3>${array[indexElementView].name}</h3>
+        <p>${array[indexElementView].description}</p>
+    </div>
+</div>
+*/
