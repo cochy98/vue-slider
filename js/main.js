d4938bacc8c4bc98f998cc8725071ca0d24bfb6f
cochy98 <cosmoferrigno@gmail.com>
2022-03-17 18:54:41 +0100

    add dinamic carousel images

--- js/main.js
+++ js/main.js
@@ -1,30 +1,55 @@
-const carouselImages = [
+const app = new Vue(
     {
-        name: 'Immagine random 1',
-        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-        source: 'https://picsum.photos/id/0/5616/3744',
-    },
-    {
-        name: 'Immagine random 2',
-        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-        source: 'https://picsum.photos/id/2/5616/3744',
-    },
-    {
-        name: 'Immagine random 3',
-        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-        source: 'https://picsum.photos/id/3/5616/3744',
-    },
-    {
-        name: 'Immagine random 4',
-        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-        source: 'https://picsum.photos/id/4/5616/3744',
-    },
-    {
-        name: 'Immagine random 5',
-        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-        source: 'https://picsum.photos/id/1/5616/3744',
-    },
-];
+        el: '#app',
+        data: {
+            activeImage: 0,
+            carouselImages: [
+                {
+                    name: 'Immagine random 1',
+                    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+                    source: 'https://picsum.photos/id/0/5616/3744',
+                },
+                {
+                    name: 'Immagine random 2',
+                    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+                    source: 'https://picsum.photos/id/2/5616/3744',
+                },
+                {
+                    name: 'Immagine random 3',
+                    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+                    source: 'https://picsum.photos/id/3/5616/3744',
+                },
+                {
+                    name: 'Immagine random 4',
+                    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+                    source: 'https://picsum.photos/id/4/5616/3744',
+                },
+                {
+                    name: 'Immagine random 5',
+                    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+                    source: 'https://picsum.photos/id/1/5616/3744',
+                },
+            ],
+        },
+        methods: {
+            previousImage: function(){
+                if(this.activeImage === 0){
+                    this.activeImage = this.carouselImages.length - 1;
+                } else{
+                    this.activeImage--;
+                }
+            },
+            nextImage: function(){
+                if (this.activeImage === this.carouselImages.length - 1){
+                    this.activeImage = 0;
+                } else{
+                    this.activeImage++;
+                }
+            }
+        },
+    }
+);
+
 
 
 /*
